306b1bac80b15cccbf5ce825d947c09c2cee09c3
fujing <1057891494@qq.com>
2018-07-10 14:48:36 +0800

    日历控件  yyyy-MM-dd改为yyyy-MM-dd hh:mm:ss

--- WebContent/fujing/#1/calendar.js
+++ WebContent/fujing/#1/calendar.js
@@ -1,5 +1,5 @@
 ;
-(function () {
+(function() {
   var calendarInputs = document.querySelectorAll(".calendar");
   if (calendarInputs.length > 0) {
     var monthList = ['壹月', '贰月', '叁月', '肆月', '伍月', '陆月', '柒月', '捌月', '玖月', '拾月', '拾壹月', '拾贰月'];
@@ -22,8 +22,8 @@
       }
       while (parent) {
         element = parent;
-        $element.$top += element.offsetTop;
-        $element.$left += element.offsetLeft;
+        $element.$top += element.offsetTop - element.scrollTop;
+        $element.$left += element.offsetLeft - element.scrollLeft;
         parent = element.offsetParent;
       }
       return $element;
@@ -31,20 +31,23 @@
 
     function edit() {
       //输入框聚焦时进入编辑模式
-      if (this.value && (/^\d{4}\/\d{2}\/\d{2}$/.test(this.value) || /^\d{8}$/.test(this.value))) {
-        this.value = this.value.split('/').join("");
+      if (this.value) {
+        if (/^\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}$/.test(this.value)) {
+          this.value = this.value.replace(/[-\s:]*/g, '');
+        }
+        return;
       } else {
-        this.value = "";
+        this.value = '';
       }
     }
 
     function format() {
-      //输入框聚焦时进入编辑模式
-      if (this.value && /^\d{4}\/\d{2}\/\d{2}$/.test(this.value) || /^\d{8}$/.test(this.value)) {
-        this.value = this.value.split('/').join("");
-        var trueDate = new Date();
-        trueDate.setFullYear(this.value.substring(0, 4), this.value.substring(4, 6) - 1, this.value.substring(6, 8));
-        this.value = trueDate.getFullYear() + '/' + ('0' + (trueDate.getMonth() - -1)).replace(/^0?(\d{2})$/, '$1') + '/' + ('0' + trueDate.getDate()).replace(/^0?(\d{2})$/, '$1');
+      //输入框失焦时
+      if (this.value) {
+        if (/^\d{14}$/.test(this.value)) {
+          this.value = this.value.replace(/^(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})$/, '$1-$2-$3 $4:$5:$6');
+        }
+        return;
       } else {
         this.value = "";
       }
@@ -66,6 +69,7 @@
       } else {
         doCancel();
       }
+      this.previousSibling.focus();
       var parent = this.parentNode;
       var calendarWindow = document.createElement("div");
       calendarWindow.className += "calendarWindow";
@@ -74,28 +78,27 @@
       calendarWindow.style.left = calendarPosition.$left + 'px';
       var nowDate = this.previousSibling.value;
       var toyear, tomonth, todate;
-      if (nowDate && /^\d{4}\/\d{2}\/\d{2}$/.test(nowDate)) {
-        toyear = nowDate.split('/')[0];
-        tomonth = nowDate.split('/')[1] - 1;
-        todate = nowDate.split('/')[2];
+      if (nowDate && /^\d{14}$/.test(nowDate)) {
+        toyear = nowDate.substring(0, 4);
+        tomonth = nowDate.substring(4, 6) - 1;
+        todate = nowDate.substring(6, 8);
+        tohour = nowDate.substring(8, 10);
+        tominute = nowDate.substring(10, 12);
+        tosecond = nowDate.substring(12, 14);
       } else {
         nowDate = new Date();
         toyear = nowDate.getFullYear();
         tomonth = nowDate.getMonth();
         todate = nowDate.getDate();
+        tohour = nowDate.getHours();
+        tominute = nowDate.getMinutes();
+        tosecond = nowDate.getSeconds();
       }
-      if (parent.lastChild == calendarInputs[i]) {
-        parent.appendChild(calendarWindow);
-        var content = document.createElement("div");
-        calendarWindow.appendChild(content);
-        setDate(toyear, tomonth, content);
-      } else {
-        parent.insertBefore(calendarWindow, this.nextSibling);
-        var content = document.createElement("div");
-        calendarWindow.appendChild(content);
-        setDate(toyear, tomonth, content, this.previousSibling);
-      }
-      //this.previousSibling.focus();
+      parent.insertBefore(calendarWindow, this.nextSibling);
+      var content = document.createElement("div");
+      calendarWindow.appendChild(content);
+      setSecond(toyear, tomonth, todate, tohour, tominute, content, this.previousSibling);
+      this.previousSibling.focus();
     }
 
     function calDays(year, month) {
@@ -110,9 +113,119 @@
       return dayList[month];
     }
 
+    function setSecond(year, month, date, hour, minute, element, target) {
+      target.focus();
+      //生成秒的html
+      element.innerHTML = "<div class='leader'><div class='left'></div><div class='timer'>" + year + "-" + ('0' + (month - -1)).replace(/^0?(\d{2})$/, '$1') + "-" + ('0' + date).replace(/^0?(\d{2})$/, '$1') + " " + ('0' + hour).replace(/^0?(\d{2})$/, '$1') + ":" + ('0' + minute).replace(/^0?(\d{2})$/, '$1') + "</div><div class='right'></div></div><div class='main'></div>";
+      var main = document.querySelector('.main');
+      var result = "";
+      for (var i = 0; i < 60; i++) {
+        result += "<div class='optionItem minuteItem' val='" + i + "'>" + (+i < 10 ? ('0' + (+i)) : +i) + "</div>";
+      }
+      main.innerHTML = result;
+      //绑定方法
+      var optionItems = document.querySelectorAll('.optionItem');
+      for (var i = 0, len = optionItems.length; i < len; i++) {
+        optionItems[i].addEventListener('click', function() {
+          target.value = year + '-' + ('0' + (month - -1)).replace(/^0?(\d{2})$/, '$1') + '-' + ('0' + date).replace(/^0?(\d{2})$/, '$1') + ' ' + ('0' + hour).replace(/^0?(\d{2})$/, '$1') + ':' + ('0' + minute).replace(/^0?(\d{2})$/, '$1') + ':' + ('0' + this.innerText).replace(/^0?(\d{2})$/, '$1');
+          doCancel();
+        });
+      }
+      document.querySelector('.left').addEventListener('click', function() {
+        year = new Date(year, month, date, hour, minute - 1).getFullYear();
+        month = new Date(year, month, date, hour, minute - 1).getMonth();
+        date = new Date(year, month, date, hour, minute - 1).getDate();
+        hour = new Date(year, month, date, hour, minute - 1).getHours();
+        minute = new Date(year, month, date, hour, minute - 1).getMinutes();
+        setSecond(year, month, date, hour, minute, element, target);
+      });
+      document.querySelector('.right').addEventListener('click', function() {
+        year = new Date(year, month, date, hour, +minute + 1).getFullYear();
+        month = new Date(year, month, date, hour, +minute + 1).getMonth();
+        date = new Date(year, month, date, hour, +minute + 1).getDate();
+        hour = new Date(year, month, date, hour, +minute + 1).getHours();
+        minute = new Date(year, month, date, hour, +minute + 1).getMinutes();
+        setSecond(year, month, date, hour, minute, element, target);
+      });
+      document.querySelector('.timer').addEventListener('click', function() {
+        setMinute(year, month, date, hour, element, target);
+      });
+    }
+
+    function setMinute(year, month, date, hour, element, target) {
+      target.focus();
+      //生成分钟的html
+      element.innerHTML = "<div class='leader'><div class='left'></div><div class='timer'>" + year + "-" + ('0' + (month - -1)).replace(/^0?(\d{2})$/, '$1') + "-" + ('0' + date).replace(/^0?(\d{2})$/, '$1') + " " + ('0' + hour).replace(/^0?(\d{2})$/, '$1') + "</div><div class='right'></div></div><div class='main'></div>";
+      var main = document.querySelector('.main');
+      var result = "";
+      for (var i = 0; i < 60; i++) {
+        result += "<div class='optionItem minuteItem' val='" + i + "'>" + (+i < 10 ? ('0' + (+i)) : +i) + "</div>";
+      }
+      main.innerHTML = result;
+      //绑定方法
+      var optionItems = document.querySelectorAll('.optionItem');
+      for (var i = 0, len = optionItems.length; i < len; i++) {
+        optionItems[i].addEventListener('click', function() {
+          setSecond(year, month, date, hour, this.attributes.val.value, element, target);
+        });
+      }
+      document.querySelector('.left').addEventListener('click', function() {
+        year = new Date(year, month, date, hour - 1).getFullYear();
+        month = new Date(year, month, date, hour - 1).getMonth();
+        date = new Date(year, month, date, hour - 1).getDate();
+        hour = new Date(year, month, date, hour - 1).getHours();
+        setMinute(year, month, date, hour, element, target);
+      });
+      document.querySelector('.right').addEventListener('click', function() {
+        year = new Date(year, month, date, +hour + 1).getFullYear();
+        month = new Date(year, month, date, +hour + 1).getMonth();
+        date = new Date(year, month, date, +hour + 1).getDate();
+        hour = new Date(year, month, date, +hour + 1).getHours();
+        setMinute(year, month, date, hour, element, target);
+      });
+      document.querySelector('.timer').addEventListener('click', function() {
+        setHour(year, month, date, element, target);
+      });
+    }
+
+    function setHour(year, month, date, element, target) {
+      target.focus();
+      //生成小时的html
+      element.innerHTML = "<div class='leader'><div class='left'></div><div class='timer'>" + year + "-" + ('0' + (month - -1)).replace(/^0?(\d{2})$/, '$1') + "-" + ('0' + date).replace(/^0?(\d{2})$/, '$1') + "</div><div class='right'></div></div><div class='main'></div>";
+      var main = document.querySelector('.main');
+      var result = "";
+      for (var i = 0; i < 24; i++) {
+        result += "<div class='optionItem hourItem' val='" + i + "'>" + (+i < 10 ? ('0' + (+i)) : +i) + "</div>";
+      }
+      main.innerHTML = result;
+      //绑定方法
+      var optionItems = document.querySelectorAll('.optionItem');
+      for (var i = 0, len = optionItems.length; i < len; i++) {
+        optionItems[i].addEventListener('click', function() {
+          setMinute(year, month, date, this.attributes.val.value, element, target);
+        });
+      }
+      document.querySelector('.left').addEventListener('click', function() {
+        year = new Date(year, month, +date - 1).getFullYear();
+        month = new Date(year, month, +date - 1).getMonth();
+        date = new Date(year, month, +date - 1).getDate();
+        setHour(year, month, date, element, target);
+      });
+      document.querySelector('.right').addEventListener('click', function() {
+        year = new Date(year, month, +date + 1).getFullYear();
+        month = new Date(year, month, +date + 1).getMonth();
+        date = new Date(year, month, +date + 1).getDate();
+        setHour(year, month, date, element, target);
+      });
+      document.querySelector('.timer').addEventListener('click', function() {
+        setDate(year, month, element, target);
+      });
+    }
+
     function setDate(year, month, element, target) {
+      target.focus();
       //生成日的html
-      element.innerHTML = "<div class='leader'><div class='left'></div><div class='timer'>" + monthList[month] + " " + year + "年" + "</div><div class='right'></div></div><div class='main'></div>";
+      element.innerHTML = "<div class='leader'><div class='left'></div><div class='timer'>" + year + "-" + ('0' + (month - -1)).replace(/^0?(\d{2})$/, '$1') + "</div><div class='right'></div></div><div class='main'></div>";
       var main = document.querySelector('.main');
       var result = "";
       for (var i = 0; i < 7; i++) {
@@ -126,7 +239,7 @@
         result += "<div class='unOptionItem'>" + (lastMonthDays - len + i - -1) + "</div>";
       }
       for (var i = 0; i < thisMonthDays; i++) {
-        result += "<div class='optionItem'>" + (i - -1) + "</div>";
+        result += "<div class='optionItem' val='" + i + "'>" + (i - -1) + "</div>";
       }
       len = 7 - (day + 6 + thisMonthDays) % 7;
       for (var i = 0; i < len; i++) {
@@ -136,29 +249,31 @@
       //绑定方法
       var optionItems = document.querySelectorAll('.optionItem');
       for (var i = 0, len = optionItems.length; i < len; i++) {
-        optionItems[i].addEventListener('click', function () {
-          target.value = year + '/' + ('0' + (month - -1)).replace(/^0?(\d{2})$/, '$1') + '/' + ('0' + this.innerText).replace(/^0?(\d{2})$/, '$1');
-          doCancel();
+        optionItems[i].addEventListener('click', function() {
+          // target.value = year + '/' + ('0' + (month - -1)).replace(/^0?(\d{2})$/, '$1') + '/' + ('0' + this.innerText).replace(/^0?(\d{2})$/, '$1');
+          setHour(year, month, this.attributes.val.value, element, target);
+          // doCancel();
         });
       }
-      document.querySelector('.left').addEventListener('click', function () {
+      document.querySelector('.left').addEventListener('click', function() {
         year = month - 1 < 0 ? year - 1 : year;
         month = month - 1 < 0 ? 11 : month - 1;
         setDate(year, month, element, target);
       });
-      document.querySelector('.right').addEventListener('click', function () {
-        year = month - -1 > 11 ? year - -1 : year;
-        month = month - -1 > 11 ? 0 : month - -1;
+      document.querySelector('.right').addEventListener('click', function() {
+        year = month > 10 ? +year + 1 : year;
+        month = month > 10 ? 0 : +month + 1;
         setDate(year, month, element, target);
       });
-      document.querySelector('.timer').addEventListener('click', function () {
+      document.querySelector('.timer').addEventListener('click', function() {
         setMonth(year, element, target);
       });
     }
 
     function setMonth(year, element, target) {
+      target.focus();
       //生成月的html
-      element.innerHTML = "<div class='leader'><div class='left'></div><div class='timer'>" + year + "年" + "</div><div class='right'></div></div><div class='main'></div>";
+      element.innerHTML = "<div class='leader'><div class='left'></div><div class='timer'>" + year + "</div><div class='right'></div></div><div class='main'></div>";
       var main = document.querySelector('.main');
       var result = "";
       for (var i = 0; i < 12; i++) {
@@ -168,24 +283,25 @@
       //绑定方法
       var optionItems = document.querySelectorAll('.monthItem');
       for (var i = 0, len = optionItems.length; i < len; i++) {
-        optionItems[i].addEventListener('click', function () {
+        optionItems[i].addEventListener('click', function() {
           setDate(year, this.attributes.val.value, element, target);
         });
       }
-      document.querySelector('.left').addEventListener('click', function () {
-        year = year - 1;
+      document.querySelector('.left').addEventListener('click', function() {
+        year = +year - 1;
         setMonth(year, element, target);
       });
-      document.querySelector('.right').addEventListener('click', function () {
-        year = year - -1;
+      document.querySelector('.right').addEventListener('click', function() {
+        year = +year + 1;
         setMonth(year, element, target);
       });
-      document.querySelector('.timer').addEventListener('click', function () {
+      document.querySelector('.timer').addEventListener('click', function() {
         setYear(year, element, target);
       });
     }
 
     function setYear(year, element, target) {
+      target.focus();
       //生成年的html
       element.innerHTML = "<div class='leader'><div class='left'></div><div class='timer'>" + Math.floor(year / 10) + "0年 - " + Math.floor(year / 10) + '9' + "年" + "</div><div class='right'></div></div><div class='main'></div>";
       var main = document.querySelector('.main');
@@ -197,16 +313,16 @@
       //绑定方法
       var optionItems = document.querySelectorAll('.yearItem');
       for (var i = 1, len = optionItems.length - 1; i < len; i++) {
-        optionItems[i].addEventListener('click', function () {
+        optionItems[i].addEventListener('click', function() {
           setMonth(this.attributes.val.value, element, target);
         });
       }
-      document.querySelector('.left').addEventListener('click', function () {
+      document.querySelector('.left').addEventListener('click', function() {
         year = year - 10;
         setYear(year, element, target);
       });
-      document.querySelector('.right').addEventListener('click', function () {
-        year = year - -10;
+      document.querySelector('.right').addEventListener('click', function() {
+        year = +year + 10;
         setYear(year, element, target);
       });
     }
@@ -219,19 +335,19 @@
       calendarImg.style.top = (elementPostition.$top - -1) + 'px';
       calendarImg.addEventListener('click', clickCalendar);
       calendarInputs[i].addEventListener('focus', edit);
-      calendarInputs[i].addEventListener('blur', format);
-      if (i == 0) {
-        document.querySelector('body').addEventListener('click', function (e) {
-          var clickTarget = e.target || window.event.target;
-          while (clickTarget.parentNode) {
-            if (clickTarget.className.indexOf('calendarWindow') != -1 || clickTarget.className.indexOf('calendarImg') != -1) {
-              return;
+      (function(target) {
+        calendarInputs[i].addEventListener('blur', function() {
+          window.setTimeout(function(){
+            if(document.activeElement===target){
+              //如果被指定了聚焦则不关闭日历弹框
+            }else{
+              format.bind(target)();
+              doCancel();
             }
-            clickTarget = clickTarget.parentNode;
-          }
-          doCancel();
-        });
-      }
+            
+          }, 200);
+        })
+      })(calendarInputs[i]);
       var parent = calendarInputs[i].parentNode;
       if (parent.lastChild == calendarInputs[i]) {
         parent.appendChild(calendarImg);
